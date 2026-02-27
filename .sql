8074da52f80a5cb0278a12890c03d925d02ac4a8
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-02-27 14:08:44 +0530

    Delete .sql

--- .sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE employees (
-    emp_id INT PRIMARY KEY,
-    emp_name VARCHAR(100),
-    department VARCHAR(50),
-    job_title VARCHAR(50),
-    salary DECIMAL(10,2),
-    performance_score INT,
-    joining_date DATE
-);
